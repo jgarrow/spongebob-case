19fe6f5fdb94e620b77db2aa647ca20762f21e8e
Janessa Garrow <janessa@apollographql.com>
2022-03-07 14:22:02 -0700

    Run linter and prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,20 +10,22 @@ describe('toSpongebobCase', () => {
   });
 
   it('parses single sentence', () => {
-    const expected = "tHiS iS a TeSt SeNtEnCe WrItTeN bY jAnE dOe, WhOeVeR tHaT iS.";
-    const sentence = "This is a test sentence written by Jane Doe, whoever that is.";
-    
+    const expected = 'tHiS iS a TeSt SeNtEnCe WrItTeN bY jAnE dOe, WhOeVeR tHaT iS.';
+    const sentence = 'This is a test sentence written by Jane Doe, whoever that is.';
+
     expect(toSpongebobCase(sentence)).toBe(expected);
     expect(toSpongebobCase(sentence.toLowerCase())).toBe(expected);
     expect(toSpongebobCase(sentence.toUpperCase())).toBe(expected);
   });
 
   it('parses a paragraph', () => {
-    const expected = "tHiS iS a TeSt PaRaGrApH wRiTtEn By JoHnNy ApPlEsEeD. hE iS a FiCtIoNaL cHaRaCtEr. It'S tHe GiRlS'. i WoNdEr WhErE tHaT sToRy CaMe FrOm.";
-    const paragraph = "This is a test paragraph written by Johnny Appleseed. He is a fictional character. It's the girls'. I wonder where that story came from.";
+    const expected =
+      "tHiS iS a TeSt PaRaGrApH wRiTtEn By JoHnNy ApPlEsEeD. hE iS a FiCtIoNaL cHaRaCtEr. It'S tHe GiRlS'. i WoNdEr WhErE tHaT sToRy CaMe FrOm.";
+    const paragraph =
+      "This is a test paragraph written by Johnny Appleseed. He is a fictional character. It's the girls'. I wonder where that story came from.";
 
     expect(toSpongebobCase(paragraph)).toBe(expected);
     expect(toSpongebobCase(paragraph.toLowerCase())).toBe(expected);
     expect(toSpongebobCase(paragraph.toUpperCase())).toBe(expected);
-  })
-})
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export function toSpongebobCase(str: string): string {
   const singleCharacter = /[a-zA-Z]/; // any word character
   const lowerCaseLetters = /[a-z]+/;
 
-  return str.split("").reduce((acc, currChar): string => {
+  return str.split('').reduce((acc, currChar): string => {
     // check if currChar is a word character
     if (singleCharacter.test(currChar)) {
       // fall back to array of empty string b/c for first character, lastMatch is undefined
@@ -11,11 +11,9 @@ export function toSpongebobCase(str: string): string {
       const lastMatch = existingWordChars[existingWordChars.length - 1];
 
       // if lastMatch is lowercase
-      return lowerCaseLetters.test(lastMatch)
-        ? acc + currChar.toUpperCase()
-        : acc + currChar.toLowerCase();
+      return lowerCaseLetters.test(lastMatch) ? acc + currChar.toUpperCase() : acc + currChar.toLowerCase();
     } else {
       return acc + currChar;
     }
-  }, "");
+  }, '');
 }
