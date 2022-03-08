1e5b9a807de32fceb8e9608db209211f76debf80
Janessa Garrow <janessa@apollographql.com>
2022-03-07 20:50:17 -0700

    Set platform in esbuild config

--- build.js
+++ build.js
@@ -9,15 +9,18 @@ const sharedConfig = {
 
 build({
   ...sharedConfig,
+  platform: 'node', // for CJS
   outfile: "dist/index.js",
 });
 
 build({
   ...sharedConfig,
   outfile: "dist/index.esm.js",
+  platform: 'neutral', // for ESM
   format: "esm",
 });
 
+// for typings
 new Generator({
   entry: 'src/index.ts',
   output: 'dist/index.d.ts',
