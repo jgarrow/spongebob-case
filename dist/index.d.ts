3959255831e4370b61c1bc40470552767c33a7b6
Janessa Garrow <janessa@apollographql.com>
2022-03-07 20:57:14 -0700

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@jgarrow/spongebob-case/index' {
-  export function toSpongebobCase(str: string): string;
-
-}
-declare module '@jgarrow/spongebob-case' {
-  import main = require('@jgarrow/spongebob-case/src/index');
-  export = main;
-}
